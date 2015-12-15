294bc208270ff7d3ee182825e9b692c59c3b1440
pannyumon <pannyumon@nainggroup.com>
2015-12-15 11:47:44 +0630

    create home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, nam laudantium laboriosam, ea optio odit error. Quam, quod, dolores. Accusamus provident at sequi, explicabo sint laboriosam minus, vel id quo!</p>
+</body>
+</html>
